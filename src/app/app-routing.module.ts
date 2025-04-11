b6ed503364300ca39ab9c58c70b47a6bd51da493
Evgeny_MKH <trisemerka@gmail.com>
2025-04-11 15:59:33 +0500

    Создание и верстка header и footer компонентов + стили

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./views/main/main.component";
+import {LayoutComponent} from "./shared/layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {path:'', component: MainComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
